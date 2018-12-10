73621569e7aaf1348ec322b04a8d724e0999ff5b
Skanarky <kutkurov@gmail.com>
2018-12-09 17:53:48 -0700

    main backend finished

--- GameOne/src/com/exercise/main/GameOne.java
+++ GameOne/src/com/exercise/main/GameOne.java
@@ -4,10 +4,15 @@
 package com.exercise.main;
 
 import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
 
 /**
+ * 
  * @author ILIAN Kutkurov
- *
+ * SINGLE threaded game
+ * 
  */
 public class GameOne extends Canvas implements Runnable {
 	
@@ -17,16 +22,77 @@ public class GameOne extends Canvas implements Runnable {
 	private static final long serialVersionUID = 870264530016327720L;
 	
 	public static final int WIDTH = 640, HEIGHT = WIDTH / 12 * 9;
+	private Thread thread;
+	private boolean running = false;
 	
 	public GameOne() {
 		new Window(WIDTH, HEIGHT, "Building My Game One", this);
 	}
 	
 	public synchronized void start() {
+		thread = new Thread(this);
+		thread.start();
 		
+		running = true;
+	}
+	
+	public synchronized void stop() {
+		try {
+			thread.join();
+			running = false;
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void run() {
+		long lastTime = System.nanoTime();
+		double amountOfTicks = 60.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		long timer = System.currentTimeMillis();
+		int frames = 0;
+		while (running) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			while (delta >= 1) {
+				tick();
+				delta--;
+			}
+			if(running)
+				render();
+			frames++;
+			
+			if(System.currentTimeMillis() - timer > 1000) {
+				timer += 1000;
+				System.out.println("FPS: " + frames);
+				frames = 0;
+			}
+		}
+		
+		stop();
+	}
+	
+	private void tick() {
+		
+	}
+	
+	private void render() {
+		BufferStrategy bs = this.getBufferStrategy();
+		
+		if(bs == null) {
+			this.createBufferStrategy(3);
+			return;
+		};
+		
+		Graphics g = bs.getDrawGraphics();
+		
+		g.setColor(Color.black);
+		g.fillRect(0, 0, WIDTH, HEIGHT);
+		
+		g.dispose();
+		bs.show();
 		
 	}
 	
