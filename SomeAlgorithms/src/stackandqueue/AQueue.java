67ea34dd1c7013a0ebb6d7e917c5c58b45d27648
Skanarky <kutkurov@gmail.com>
2019-03-24 17:48:09 -0600

    exerc stack and q fin

--- SomeAlgorithms/src/stackandqueue/AQueue.java
+++ SomeAlgorithms/src/stackandqueue/AQueue.java
@@ -1,9 +1,142 @@
 package stackandqueue;
 
+import java.util.Arrays;
+
 public class AQueue {
+	
+	private String[] queueAsArrStr;
+	private int queueSize;
+	
+	// we need a front, rear and numberOfItems to execute FIFO
+	private int front, rear, numberOfItems = 0;
+	
+	AQueue(int size) {
+		this.queueSize = size;
+		this.queueAsArrStr = new String[size];
+		
+		Arrays.fill(this.queueAsArrStr, "-1");
+	}
+	
+	public void insert(String val) {
+		if (this.numberOfItems + 1 <= this.queueSize) {
+			
+			this.queueAsArrStr[this.rear] = val;
+			this.rear++;
+			this.numberOfItems++;
+			
+			System.out.println(val + " was added!");
+			
+		} else System.out.println("No room in the queue for: " + val);
+		
+		this.displayTheQueue();
+	}
+	
+	public void remove() {
+		if (this.numberOfItems > 0) {
+			
+			System.out.println(this.queueAsArrStr[this.front] + " was removed!");
+			this.queueAsArrStr[this.front] = "-1";
+			this.front++;
+			this.numberOfItems--;
+			
+		} else System.out.println("No items in the queue.");
+		
+		this.displayTheQueue();
+	}
+	
+	// from high to low -> FIFO
+	public void priorityInsert(String val) {
+		int i = this.rear - 1;
+		
+		if (this.numberOfItems + 1 <= this.queueSize) {
+			
+			if (this.numberOfItems == 0) {
+				this.insert(val);
+			} else {
+				
+				for(i = this.rear - 1; i >= 0; --i) {
+					
+					if(Integer.parseInt(val) > Integer.parseInt(this.queueAsArrStr[i])) {
+						this.queueAsArrStr[i + 1] = this.queueAsArrStr[i];
+					} else break;
+					
+				}
+				
+				System.out.println(val + " was added!");
+				
+				this.queueAsArrStr[i + 1] = val;
+				this.rear++;
+				this.numberOfItems++;
+				
+			}
+			
+		} else System.out.println("No room in the queue for: " + val); 
+		
+		this.displayTheQueue();
+		
+	}
+	
+	public void peek() {
+		if (this.numberOfItems > 0) {
+			
+			System.out.println("The value of the queue at the first position " + this.front + " is: " + this.queueAsArrStr[this.front]);
+			
+		} else System.out.println("No items in the queue.");
+		
+		this.displayTheQueue();
+	}
+	
+	public void displayTheQueue(){
+		
+		System.out.print(" ");
+		for(int i = 0; i < 59; ++i) System.out.print("-");
+		
+		System.out.println();
+		
+		for(int i = 0; i < this.queueSize; ++i){
+			System.out.format("| %2s "+ " ", i);
+		}
+		
+		System.out.println("|");
+		
+		System.out.print(" ");
+		for(int i = 0; i < 59; ++i) System.out.print("-");
+		
+		System.out.println();
+		
+		for(int i = 0; i < this.queueSize; ++i){
+			if(this.queueAsArrStr[i].equals("-1")) System.out.print("|     ");
+			else System.out.print(String.format("| %2s "+ " ", this.queueAsArrStr[i]));
+		}
+		
+		System.out.println("|");
+		
+		System.out.print(" ");
+		for(int i = 0; i < 59; ++i) System.out.print("-");
+		
+		System.out.println();
+		
+	}
 
 	public static void main(String[] args) {
-		System.out.println("hi queue");
+		
+		AQueue aQueue = new AQueue(10);
+		
+//		aQueue.insert("5");
+//		aQueue.insert("8");
+//		aQueue.insert("1");
+		
+		aQueue.priorityInsert("5");
+		aQueue.priorityInsert("8");
+		aQueue.priorityInsert("4");
+		aQueue.priorityInsert("9");
+		
+		aQueue.peek();
+//		aQueue.remove();
+//		aQueue.remove();
+//		aQueue.remove();
+//		aQueue.peek();
+		
 	}
 
 }
--- SomeAlgorithms/src/stackandqueue/AStack.java
+++ SomeAlgorithms/src/stackandqueue/AStack.java
@@ -1,9 +1,126 @@
 package stackandqueue;
 
+import java.util.Arrays;
+
 public class AStack {
+	
+	private String[] stackAsArrStr;
+	private int stackSize;
+	
+	// we need a top to execute LIFO
+	private int stackTop = -1;
+	
+	AStack(int size) {
+		this.stackSize = size;
+		this.stackAsArrStr = new String[size];
+		
+		Arrays.fill(this.stackAsArrStr, "-1");
+	}
+	
+	public void push(String val) {
+		if (this.stackTop + 1 < this.stackSize) {
+			
+			this.stackTop++;
+			this.stackAsArrStr[this.stackTop] = val;
+			
+			System.out.println(val + " was added!");
+			
+		} else System.out.println("No room in the stack for: " + val);
+		
+		this.displayTheStack();
+	}
+	
+	public void pop() {
+		if (this.stackTop >= 0) {
+			
+			System.out.println(this.stackAsArrStr[this.stackTop] + " was removed!");
+			this.stackAsArrStr[this.stackTop] = "-1";
+			this.stackTop--;
+			
+		} else System.out.println("No items in the stack.");
+		
+		this.displayTheStack();
+	}
+	
+	public void peek() {
+		if (this.stackTop >= 0) {
+			
+			System.out.println("The value of the stack at the top position " + this.stackTop + " is: " + this.stackAsArrStr[this.stackTop]);
+			
+		} else System.out.println("No items in the stack.");
+		
+		this.displayTheStack();
+	}
+	
+	public void pushMultiVal(String inp) {
+		String[] arrInp = inp.split(" ");
+		
+		for(String i : arrInp) {
+			this.push(i);
+		}
+	}
+	
+	public void popAllVals() {
+		if (this.stackTop >= 0) {
+			
+			for(int i = this.stackTop; i >= 0; --i) {
+				this.pop();
+			}
+			
+			this.stackTop = -1;
+			
+		} else System.out.println("No items in the stack.");
+		
+	}
+	
+	public void displayTheStack(){
+		
+		System.out.print(" ");
+		for(int i = 0; i < 59; ++i) System.out.print("-");
+		
+		System.out.println();
+		
+		for(int i = 0; i < this.stackSize; ++i){
+			System.out.format("| %2s "+ " ", i);
+		}
+		
+		System.out.println("|");
+		
+		System.out.print(" ");
+		for(int i = 0; i < 59; ++i) System.out.print("-");
+		
+		System.out.println();
+		
+		for(int i = 0; i < this.stackSize; ++i){
+			if(this.stackAsArrStr[i].equals("-1")) System.out.print("|     ");
+			else System.out.print(String.format("| %2s "+ " ", this.stackAsArrStr[i]));
+		}
+		
+		System.out.println("|");
+		
+		System.out.print(" ");
+		for(int i = 0; i < 59; ++i) System.out.print("-");
+		
+		System.out.println();
+		
+	}
+
 
 	public static void main(String[] args) {
-		System.out.println("hi stack");
+
+		AStack aStack = new AStack(10);
+		aStack.push("5");
+		aStack.push("9");
+		aStack.push("7");
+		aStack.push("1");
+//		aStack.pop();
+		aStack.peek();
+//		aStack.popAllVals();
+		aStack.pushMultiVal("2 4 6 10 3 8 11");
+		aStack.peek();
+		aStack.popAllVals();
+		aStack.peek();
+		
 	}
 
 }
