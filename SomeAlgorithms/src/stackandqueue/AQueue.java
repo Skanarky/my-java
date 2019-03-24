2717e4f45e7bfd9d7698bf8c29763c34b5aa7117
Skanarky <kutkurov@gmail.com>
2019-03-24 14:48:31 -0600

    started stack and queue exerc

---
+++ SomeAlgorithms/src/stackandqueue/AQueue.java
@@ -0,0 +1,9 @@
+package stackandqueue;
+
+public class AQueue {
+
+	public static void main(String[] args) {
+		System.out.println("hi queue");
+	}
+
+}
---
+++ SomeAlgorithms/src/stackandqueue/AStack.java
@@ -0,0 +1,9 @@
+package stackandqueue;
+
+public class AStack {
+
+	public static void main(String[] args) {
+		System.out.println("hi stack");
+	}
+
+}
