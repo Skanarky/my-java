fea00a4ff76f2179f8bfdc0a598a0e0ddd7e9ef5
Skanarky <kutkurov@gmail.com>
2019-04-09 15:43:07 -0600

    LL first session

--- SomeAlgorithms/src/linkedlist/Link.java
+++ SomeAlgorithms/src/linkedlist/Link.java
@@ -1,9 +1,47 @@
 package linkedlist;
 
 public class Link {
+	
+	public String teamPlayerName;
+	public int yearsExperience;
+	
+	public Link next;
+	
+	public Link(String name, int years) {
+		this.teamPlayerName = name;
+		this.yearsExperience =  years;
+	}
+	
+	public void displayLink() {
+		System.out.println("Player: " + this.teamPlayerName + ";");
+		System.out.println("Years: " + this.yearsExperience);
+		System.out.println("");
+	}
+	
+	public String toString() {
+		return this.teamPlayerName;
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		LinkList oneLinkedList = new LinkList();
+		
+		oneLinkedList.insertFirstLink("David", 3);
+		oneLinkedList.insertFirstLink("John", 1);
+		oneLinkedList.insertFirstLink("Susy", 5);
+		oneLinkedList.insertFirstLink("Jake", 4);
+		
+//		oneLinkedList.displayLinkList();
+		
+		System.out.println(oneLinkedList.findALink("John"));
+		
+		oneLinkedList.removeFirstLink();
+//		System.out.println(oneLinkedList.findALink("Jake"));
+		
+		oneLinkedList.removeALink("John");
+		System.out.println(oneLinkedList.findALink("John"));
+		
+		oneLinkedList.displayLinkList();
 
 	}
 
--- SomeAlgorithms/src/linkedlist/LinkList.java
+++ SomeAlgorithms/src/linkedlist/LinkList.java
@@ -1,5 +1,143 @@
 package linkedlist;
 
 public class LinkList {
+	
+	// this is the last item added to the list
+	// it's called first because it's the first to go (stack?!)
+	public Link firstLink;
+	
+	public LinkList() {
+		
+		this.firstLink = null;
+		
+	}
+	
+	public boolean isListEmpty() {
+		
+		return (this.firstLink == null);
+		
+	}
+	
+	public void insertFirstLink(String name, int years) {
+		
+		Link firstOne = new Link(name, years);
+		
+		firstOne.next = this.firstLink;
+		
+		this.firstLink = firstOne;
+		
+	}
+	
+	public Link removeFirstLink() {
+		
+		Link removedLink = this.firstLink;
+		
+		if (!this.isListEmpty()) {
+			
+			this.firstLink = this.firstLink.next;
+			
+		} else {
+			
+			System.out.println("List is empty.");
+			
+		}
+		
+		return removedLink;
+		
+	}
+	
+	public void displayLinkList() {
+		
+		Link oneLink = this.firstLink;
+		
+		if (!this.isListEmpty()) {
+			
+			while (oneLink != null) {
+				
+				oneLink.displayLink();
+				
+				oneLink = oneLink.next;
+				
+			}
+			
+		} else {
+			
+			System.out.println("List is empty.");
+			
+		}
+		
+	}
+	
+	public Link findALink(String name) {
+		
+		Link oneLink = this.firstLink;
+		
+		if (!this.isListEmpty()) {
+			
+			while (oneLink.teamPlayerName != name) {
+				
+				if (oneLink.next == null) {
+					
+					return null;
+					
+				} else {
+					
+					oneLink = oneLink.next;
+					
+				}
+				
+			}
+			
+		} else {
+			
+			return null;
+			
+		}
+		
+		return oneLink;
+		
+	}
+	
+	public Link removeALink(String name) {
+		
+		Link currentLink = this.firstLink;
+		Link previousLink = this.firstLink;
+		
+		if (!this.isListEmpty()) {
+			
+			while (currentLink.teamPlayerName != name) {
+				
+				if (currentLink.next == null) {
+					
+					return null;
+					
+				} else {
+					
+					previousLink = currentLink;
+					currentLink = currentLink.next;
+					
+				}
+				
+			}
+			
+			if (currentLink == this.firstLink) {
+				
+				this.firstLink = this.firstLink.next;
+				
+			} else {
+				
+				previousLink.next = currentLink.next;
+				
+			}
+			
+		} else {
+			
+			return null;
+			
+		}
+		
+		return currentLink;
+		
+	}
 
 }
