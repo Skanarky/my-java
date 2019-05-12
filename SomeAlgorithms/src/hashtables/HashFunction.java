2363b774f7b5c75c57c29ce08437fdd3747b52dc
Skanarky <kutkurov@gmail.com>
2019-05-11 18:17:10 -0600

    hashtables part 1

--- SomeAlgorithms/src/hashtables/HashFunction.java
+++ SomeAlgorithms/src/hashtables/HashFunction.java
@@ -31,15 +31,17 @@ public class HashFunction {
 		
 		for (String i : inputStrings) {
 			
+			// hashed index
 			// modulus of 29 -> the size of our arr is 30
 			int indexToInsert = Integer.parseInt(i) % 29;
 			
 			while (theArr[indexToInsert] != "-1") {
 				
 				++indexToInsert;
+				System.out.println("Collision, will try index: " + indexToInsert + " instead!");
 				
-				
-				
+				// if it gets to arrSize -> go back to 0 index
+				indexToInsert %= this.arrSize;
 			}
 			
 			theArr[indexToInsert] = i;
@@ -47,15 +49,57 @@ public class HashFunction {
 		}
 		
 	}
+	
+	public String getKey(String key) {
+		
+		// hashed index
+		int indexToInsert = Integer.parseInt(key) % 29;
+		
+		while (this.anArray[indexToInsert] != "-1") {
+			
+			if (this.anArray[indexToInsert] == key) {
+				
+				System.out.println(key + " was found in item with index " + indexToInsert);
+				
+				return this.anArray[indexToInsert];
+
+			}
+			
+			
+			++indexToInsert;
+			
+			if (indexToInsert == this.arrSize) {
+				
+				break;
+				
+			}
+
+			System.out.println("Not found with regular hash, will try next index: " + indexToInsert + " instead!");
+
+		}
+		
+		System.out.println("Key " + key + " wasn't found in the Hash Table!");
+		return null;
+		
+	}
 
 	public static void main(String[] args) {
 		
 		HashFunction oneHFunc = new HashFunction(30);
-		String[] strToAdd = {"12", "5", "19", "1", "28"};
-		
-		oneHFunc.hashFSimpleStoreIdSameInd(strToAdd, oneHFunc.anArray);
-		
+
+//		String[] strToAdd = {"12", "5", "19", "1", "28"};
+//		oneHFunc.hashFSimpleStoreIdSameInd(strToAdd, oneHFunc.anArray);
+
+		String[] strToAdd2 = { "100", "510", "170", "214", "268", "398",
+			    "235", "802", "900", "723", "699", "1", "16", "999", "890",
+			    "725", "998", "978", "988", "990", "989", "984", "320", "321",
+			    "400", "415", "450", "50", "660", "624" };
+		oneHFunc.hashFStoreIdCalcByMod(strToAdd2, oneHFunc.anArray);
+
 		oneHFunc.displayTheHashF();
+		
+		oneHFunc.getKey("999");
+		oneHFunc.getKey("401");
 
 	}
 	
