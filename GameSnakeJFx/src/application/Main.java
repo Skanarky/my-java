8d3207ee1daf3cfc7c491660273fa0c71ba0b75c
Skanarky <kutkurov@gmail.com>
2019-06-29 10:26:40 -0600

    added anim and main mech snake

--- GameSnakeJFx/src/application/Main.java
+++ GameSnakeJFx/src/application/Main.java
@@ -4,9 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.VBox;
 
 
@@ -29,6 +32,35 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			VBox root = new VBox();
+			Canvas canvas = new Canvas(width * bodyPartSize, height * bodyPartSize);
+			GraphicsContext grCtxt = canvas.getGraphicsContext2D();
+			root.getChildren().add(canvas);
+			
+			// this is the flip book
+			new AnimationTimer() {
+
+				long lastTick = 0;
+
+				public void handle(long now) {
+
+					if (lastTick == 0) {
+
+						lastTick = now;
+
+						return;
+
+					}
+
+					// frames every speed second -> more frames the snake is faster
+					if ((now - lastTick) > (1000000000 / speed)) {
+
+						lastTick = now;
+
+					}
+
+				}
+
+			}.start();
 			
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
@@ -37,6 +69,27 @@ public class Main extends Application {
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static void makeFood() {
+		
+		start: while(true) {
+
+			foodX = random.nextInt(width);
+			foodY = random.nextInt(height);
+			
+			for(BodyPart bp : theSnake) {
+				if (bp.x == foodX && bp.y == foodY) {
+					continue start;
+				}
+			}
+			
+			foodColor = random.nextInt(5);
+			++speed;
+			break;
+
+		}
+		
 	}
 	
 	public static void main(String[] args) {
