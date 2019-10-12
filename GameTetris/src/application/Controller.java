9460e88706a5ac0d3c3f51fda9e19a459030dd38
Skanarky <kutkurov@gmail.com>
2019-10-12 11:26:24 -0600

    fin controller

--- GameTetris/src/application/Controller.java
+++ GameTetris/src/application/Controller.java
@@ -51,14 +51,94 @@ public class Controller {
 		
 		// random color
 		int block = (int) (Math.random() * 100);
+
 		String name;
 		
 		Rectangle a = new Rectangle(SIZE -1, SIZE -1),
-					b = new Rectangle(SIZE -1, SIZE -1),
-					c = new Rectangle(SIZE -1, SIZE -1),
-					d = new Rectangle(SIZE -1, SIZE -1);
+			b = new Rectangle(SIZE -1, SIZE -1),
+			c = new Rectangle(SIZE -1, SIZE -1),
+			d = new Rectangle(SIZE -1, SIZE -1);
+		
+		// I THINK THE BLOCKS ARE WRONG BELOW
+		
+		// "j" block
+		if (block < 15) {
+			a.setX(MAXX / 2 - SIZE);
+			b.setX(MAXX / 2 - SIZE);
+			b.setY(SIZE);
+			c.setX(MAXX / 2);
+			c.setY(SIZE);
+			d.setX(MAXX / 2 + SIZE);
+			d.setY(SIZE);
+			
+			name = "j";
 
-		// ...
+		// "l" block
+		} else if (block < 30) {
+			a.setX(MAXX / 2 + SIZE);
+			b.setX(MAXX / 2 - SIZE);
+			b.setY(SIZE);
+			c.setX(MAXX / 2);
+			c.setY(SIZE);
+			d.setX(MAXX / 2 + SIZE);
+			d.setY(SIZE);
+			
+			name = "l";
+		// "o" block
+		} else if (block < 45) {
+			a.setX(MAXX / 2 - SIZE);
+			b.setX(MAXX / 2);
+			b.setX(SIZE / 2 - SIZE);
+			c.setY(SIZE);
+			d.setX(MAXX / 2);
+			d.setY(SIZE);
+			
+			name = "o";
+	
+		// "s" block
+		} else if (block < 60) {
+			a.setX(MAXX / 2 + SIZE);
+			b.setX(MAXX / 2);
+			b.setX(MAXX / 2);
+			c.setY(SIZE);
+			d.setX(MAXX / 2 - SIZE);
+			d.setY(SIZE);
+			
+			name = "s";
+	
+		// "t" block
+		} else if (block < 75) {
+			a.setX(MAXX / 2 - SIZE);
+			b.setX(MAXX / 2);
+			b.setX(MAXX / 2);
+			c.setY(SIZE);
+			d.setX(MAXX / 2 + SIZE);
+			
+			name = "t";
+	
+		// "z" block
+		} else if (block < 90) {
+			a.setX(MAXX / 2 + SIZE);
+			b.setX(MAXX / 2);
+			b.setX(MAXX / 2 + SIZE);
+			c.setY(SIZE);
+			d.setX(MAXX / 2 + SIZE + SIZE);
+			d.setY(SIZE);
+			
+			name = "z";
+
+		// "i" block	
+		} else {
+			a.setX(MAXX / 2 - SIZE - SIZE);
+			b.setX(MAXX / 2 - SIZE);
+			b.setX(MAXX / 2);
+			d.setX(MAXX / 2 + SIZE);
+			
+			name = "i";
+			
+		}
+		
+		return new Form(a, b, c, d, name);
 
 	}
 
